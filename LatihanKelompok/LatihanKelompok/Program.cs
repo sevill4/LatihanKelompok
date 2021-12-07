2b1fb614022154a6869d83f0a3ea023cf4061046
Sevilla Alfataah <sevillaalfataah21@gmail.com>
2021-12-07 22:15:01 +0700

    latihan kelompok

---
+++ LatihanKelompok/LatihanKelompok/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LatihanKelompok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
